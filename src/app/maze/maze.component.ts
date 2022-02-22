bae64fb8ab790292419e39778c24d52bdd501fee
Benjamin <insignificantterrestrial@gmail.com>
2022-02-22 13:14:05 -0500

    add algorithm choices and improve interface

--- src/app/maze/maze.component.ts
+++ src/app/maze/maze.component.ts
@@ -15,15 +15,19 @@ const delay = (delayInms:number) => {
 })
 
 export class MazeComponent implements OnInit {
-  height:number = 15;
+  height:number = 13;
 
-  width:number = 15;
+  width:number = 28;
+
+  algorithm:string = "A*";
+
+  algorithms:string[] = ["A*", "Dijkstra"];
 
   squares:number[][] = this.createSquares();
 
-  start:number[] = [];
+  start:number[] = [6, 14];
 
-  end:number[] = [];
+  end:number[] = [12, 27];
 
   selected:number[][] = [];
 
@@ -45,11 +49,11 @@ export class MazeComponent implements OnInit {
 
   zoom:number = 100;  
 
-  zooms:number[] = [200, 150, 125, 100, 50, 25]; 
+  zooms:number[] = [200, 175, 150, 125, 100, 75, 50, 25]; 
 
   branchingPaths: {
-    [key: number]: number[]
-  } = [];
+    [key: string]: number[]
+  } = {};
   
   isSolving:boolean = false;
 
@@ -95,6 +99,14 @@ export class MazeComponent implements OnInit {
       const target = event.target as HTMLTextAreaElement;
   
       this.speed = parseInt(target.value);
+    }
+  }
+
+  setAlgorithm(event:Event) {
+    if (!this.isSolving) {
+      const target = event.target as HTMLTextAreaElement;
+  
+      this.algorithm = target.value;
     }
   }
 
@@ -163,92 +175,6 @@ export class MazeComponent implements OnInit {
     }
   }
 
-  findNeighbors(point:number[]) {
-    const directions = [[1, 0], [0, 1], [-1, 0], [0, -1]];
-    const neighbors:number[][] = [];
-
-    directions.forEach(a => {
-      const neighbor = [(a[0] + point[0]), (a[1] + point[1])];
-      
-      if ((neighbor[0] >= 0 && neighbor[0] <= (this.height - 1) && neighbor[1] >= 0 && neighbor[1] <= (this.width - 1)) && !this.isPositionPresent(neighbor)) {
-        neighbors.push(neighbor);
-      }
-    });
-
-    return neighbors;
-  }
-
-  filterNeighbors() {
-    const adjacentSquares = this.findNeighbors(this.currentPosition);
-    
-    adjacentSquares.forEach(neighbor => {
-      if (!(this.isPresent(this.seen, neighbor) || this.isPresent(this.possiblePositions, neighbor))) {
-        this.possiblePositions.push(neighbor);
-        this.branchingPaths[this.calcInd(neighbor)] = this.currentPosition;
-      }
-    });
-  }
-
-  async solve() {
-    if (this.start.length === 2 && this.end.length === 2 && !this.isSolving) {
-      this.clearPath();
-
-      this.isSolving = true;
-      this.currentPosition = this.start;
-      this.possiblePositions = [this.start];
-      this.seen = [this.start];
-  
-      while (this.possiblePositions.length !== 0 && !this.areSamePos(this.currentPosition, this.end)) {
-
-        if (this.speed !== 0) {
-          let delayRes = await delay(this.speed);
-        }
-
-        this.currentPosition = this.bestPosFScore(this.possiblePositions);
-
-        const posInd = this.possiblePositions.findIndex(ele => this.areSamePos(ele, this.currentPosition));
-        
-        this.possiblePositions.splice(posInd, 1);
-
-        this.seen.push(this.currentPosition);
-
-        this.filterNeighbors();
-      }
-  
-      this.finalPath = this.findPath();
-      
-      this.isSolving = false;
-    }
-  }
-
-  bestPosFScore(positions:number[][]) {
-    // pick the position with the best fScore
-    return positions.reduce((chosenPoint, point) => {
-      const oldFScore = this.manhattanDistance(chosenPoint);
-      const newFScore = this.manhattanDistance(point);
-
-      if (oldFScore > newFScore) {
-        return point;
-      } else {
-        return chosenPoint;
-      }
-    });
-  }
-
-  manhattanDistance(pos:number[]) {
-    // f = g + h
-
-    // lowest fscore is best path
-
-    return (
-      // gscore = distance between the current node and the start node
-      this.findPath(pos).length 
-      + 
-      // hscore = estimated distance from the current node to the end node.
-      (Math.abs(pos[0] - this.end[0]) + Math.abs(pos[1] - this.end[1]))
-    );
-  }
-
   setDimensions(option:{event:Event | undefined, number:number | undefined}, dim:string) {
     if (!this.isSolving) {
       let value:number;
@@ -276,17 +202,139 @@ export class MazeComponent implements OnInit {
     }
   }
 
+  findNeighbors(position:number[]) {
+    const directions = [[1, 0], [0, 1], [-1, 0], [0, -1]];
+    const neighbors:number[][] = [];
+
+    directions.forEach(direction => {
+      const neighbor = [(direction[0] + position[0]), (direction[1] + position[1])];
+      
+      if ((neighbor[0] >= 0 && neighbor[0] <= (this.height - 1) && neighbor[1] >= 0 && neighbor[1] <= (this.width - 1)) && !this.isPositionPresent(neighbor)) {
+        neighbors.push(neighbor);
+      }
+    });
+
+    return neighbors;
+  }
+
+  filterNeighbors() {
+    const adjacentSquares = this.findNeighbors(this.currentPosition);
+    
+    adjacentSquares.forEach(neighbor => {
+      if (!(this.isPresent(this.seen, neighbor) || this.isPresent(this.possiblePositions, neighbor))) {
+        this.possiblePositions.push(neighbor);
+        this.branchingPaths[neighbor.toString()] = this.currentPosition;
+      }
+    });
+  }
+
+  async solve() {
+    if (this.start.length === 2 && this.end.length === 2 && !this.isSolving) {
+      this.clearPath();
+
+      this.isSolving = true;
+      this.currentPosition = this.start;
+      this.possiblePositions = [this.start];
+      this.seen = [this.start];
+  
+      while (this.possiblePositions.length !== 0 && !this.areSamePos(this.currentPosition, this.end)) {
+
+        if (this.speed !== 0) {
+          let delayRes = await delay(this.speed);
+        }
+
+
+        if (this.algorithm === "A*") {
+          // a* algorithm (informed)
+          this.currentPosition = this.bestPosFScore(this.possiblePositions);
+        } else {
+          // Dijkstra’s algorithm (uninformed)
+          this.currentPosition = this.reduceArray(this.possiblePositions, (position:number[]) => this.findPath(position).length);
+        }
+
+        const posInd = this.possiblePositions.findIndex(ele => this.areSamePos(ele, this.currentPosition));
+        
+        this.possiblePositions.splice(posInd, 1);
+
+        this.seen.push(this.currentPosition);
+
+        this.filterNeighbors();
+      }
+  
+      this.finalPath = this.findPath();
+      
+      this.isSolving = false;
+    }
+  }
+
+  bestPosFScore(positions:number[][]) {
+    // pick the position with the best fScore
+
+    return this.reduceArray(positions, (position:number[]) => this.manhattanDistance(position));
+  }
+
+  reduceArray(array:number[][], reducer:Function) {
+    return array.reduce((prevPos, currentPos) => {
+      const oldFScore = reducer(prevPos);
+      const newFScore = reducer(currentPos);
+
+      if (oldFScore > newFScore) {
+        return currentPos;
+      } else {
+        return prevPos;
+      }
+    });
+  }
+
+  manhattanDistance(pos:number[]) {
+    // f = g + h
+
+    // lowest fscore is best path
+
+    return (
+      // gscore = distance between the current node and the start node
+      this.findPath(pos).length 
+      + 
+      // hscore = estimated distance from the current node to the end node.
+      (Math.abs(pos[0] - this.end[0]) + Math.abs(pos[1] - this.end[1]))
+    );
+  }
+
   findPath(goal = this.end) {
     const path:number[][] = [goal];
-    let spot:number[] = goal;
+    let spot:string = goal.toString();
 
-    while (this.branchingPaths[this.calcInd(spot)] !== undefined) {
-      // keep pushing the positions until we hit the goal
-      path.push(this.branchingPaths[this.calcInd(spot)]);
-      spot = this.branchingPaths[this.calcInd(spot)];
+    while (this.branchingPaths[spot] !== undefined) {
+      // keep pushing the positions until we hit undefined
+
+      // this loop works by using the value of the current goal to give us the path that led us from the starting position to the current goal
+
+      // if our goal is [12, 27], we will start off of [12, 27] which the value then leads us to [12, 26]
+      // which then leads us to the key [12, 26] which then gives us the value [12, 25] and then we keep doing this 
+      // working our way backwards until we reach a key-value pair where the value is not a key in branchingPaths 
+      // which means we've reached the beginning (or starting position)
+      // {
+      //   [12,14]: [11, 14],
+      //   [12,15]: [12, 14],
+      //   [12,16]: [12, 15],
+      //   [12,17]: [12, 16],
+      //   [12,18]: [12, 17],
+      //   [12,19]: [12, 18],
+      //   [12,20]: [12, 19],
+      //   [12,21]: [12, 20],
+      //   [12,22]: [12, 21],
+      //   [12,23]: [12, 22],
+      //   [12,24]: [12, 23],
+      //   [12,25]: [12, 24],
+      //   [12,26]: [12, 25],
+      //   [12,27](goal): [12, 26]
+      // }
+
+      path.push(this.branchingPaths[spot]);
+      spot = this.branchingPaths[spot].toString();
     }
 
-    // the returned value will tell us what nodes were taken to reach the goal
+    // the returned value will tell us what positions were taken to reach the current goal
     return path;
   }
 }
