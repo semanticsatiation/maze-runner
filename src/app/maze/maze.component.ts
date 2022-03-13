9baa2b5762e30ece17449b29cf5d5ef72997d5de
Benjamin <insignificantterrestrial@gmail.com>
2022-03-12 19:15:03 -0500

    fix a* algorithm and add BFS algorithm, weights, and stats

--- src/app/maze/maze.component.ts
+++ src/app/maze/maze.component.ts
@@ -8,6 +8,36 @@ const delay = (delayInms:number) => {
   });
 };
 
+const defaultNode = {
+  f: 999, 
+  g: 999, 
+  h: 999,
+  parent: undefined, 
+  seen: false, 
+  final: false,
+  closed: false,
+  wall: false,
+  // weight cost is 20
+  weight: false
+};
+
+interface NodeObj {
+  pos: number[],
+  f: number, 
+  g: number, 
+  h: number,
+  parent?: NodeObj, 
+  seen: boolean, 
+  final: boolean,
+  closed: boolean,
+  wall: boolean,
+  weight: boolean
+}
+
+interface MiniNodeObj {
+  pos: number[],
+};
+
 @Component({
   selector: 'app-maze',
   templateUrl: './maze.component.html',
@@ -15,55 +45,74 @@ const delay = (delayInms:number) => {
 })
 
 export class MazeComponent implements OnInit {
-  height:number = 10;
+  height:number = 20;
 
-  width:number = 10;
+  width:number = 20;
 
   algorithm:string = "A*";
 
-  algorithms:string[] = ["A*", "Dijkstra"];
+  algorithms:string[] = ["A*", "Dijkstra", "Breadth First Search"];
 
-  squares:number[][] = this.createSquares();
+  squares: NodeObj[][] = [];
 
   start:number[] = [];
 
   end:number[] = [];
 
-  selected:number[][] = [];
+  openList:NodeObj[] = [];
 
-  seen:number[][] = [];
-
-  finalPath:number[][] = [];
-
-  possiblePositions:number[][] = [];
-
-  currentPosition:number[] = [];
+  currentNode:NodeObj = {pos: [], ...defaultNode};
 
   mouseIsDown = false;
 
-  clicked = false;
+  clicked:boolean = false;
+  
+  speeds:[string, string][] = [["Slow", "150"], ["Normal", "40"], ["Fast", "1"], ["Instant", "0"]];
 
-  speed:number = 20;
-
-  speeds:any[] = [["Slow", "150"], ["Normal", "20"], ["Fast", "1"], ["Instant", "0"]];
+  speed:number = 40;
 
   zoom:number = 100;  
 
   zooms:number[] = [200, 175, 150, 125, 100, 75, 50, 25]; 
 
-  branchingPaths: {
-    [key: string]: number[]
-  } = {};
+  currentTileType:string = "walls";
   
   isSolving:boolean = false;
 
-  constructor() { }
+  seenTotal:number = 0;
+
+  wallsTotal:number = 0;
+
+  seenTotalPercentage:number = 0;
+
+  finalTotal:number = 0;
+
+  totalSteps:number = 0;
+
+  timeTaken:number = 0;
 
   ngOnInit(): void {
+    this.createSquares();
+  }
+
+  createSquares() {
+    this.clearMaze();
+
+    this.squares = [...Array(this.height)].map((item, ri) => (
+      [...Array(this.width)].map((item, ci) => {
+        return {...defaultNode, pos: [ri, ci]};
+      })
+    ));
   }
 
   calcZoom() {
     return ((this.zoom / 100) * 17) + "px";
+  }
+
+  changeTileType(event:Event) {
+    const target = event.target as HTMLTextAreaElement;
+
+    this.currentTileType = target.value;
   }
 
   adjustZoom(event:Event) {
@@ -72,26 +121,18 @@ export class MazeComponent implements OnInit {
     this.zoom = parseInt(target.value);
   }
 
-  calcInd(arr:number[]) {
-    return (arr[0] * this.width) + arr[1];
+  isPresent(arrOfObjs:NodeObj[] | MiniNodeObj[], target:NodeObj | MiniNodeObj) {
+    return arrOfObjs.find((ele: NodeObj | MiniNodeObj) => this.areSamePos(ele, target)) !== undefined;
   }
 
-  areSamePos(arrOne:number[], arrTwo:number[]) {
-    return this.calcInd(arrOne) === this.calcInd(arrTwo);
+  areSamePos(eleObj: NodeObj | MiniNodeObj, target:NodeObj | MiniNodeObj) {
+    return eleObj.pos.toString() === target.pos.toString();
   }
 
-  clickToggle(array:[number, number]) {
+  clickToggle(node:NodeObj) {
     this.clicked = true;
 
-    this.toggleTile(array);
-  }
-
-  createSquares() {
-    this.clearMaze();
-
-    return this.squares = [...Array(this.height)].map((item, ind) => (
-      [...Array(this.width)].map((item, ind) => 0)
-    ));
+    this.toggleTile(node);
   }
 
   adjustSpeed(event:Event) {
@@ -110,39 +151,32 @@ export class MazeComponent implements OnInit {
     }
   }
 
-  toggleTile(array:[number, number]) {
-    if (!this.isSolving && (this.mouseIsDown || this.clicked) && !this.areSamePos(array, this.start) && !this.areSamePos(array, this.end)) {
-      const newArr = [...this.selected];
-
-      if (this.isPresent(newArr, array)) {
-        const eleIndex = newArr.findIndex(ele => this.areSamePos(ele, array));
-
-        if (eleIndex !== -1) {
-          newArr.splice(eleIndex, 1);
-        }
-      } else {
-        newArr.push(array);
-      }
-
+  toggleTile(node:NodeObj) {
+    if (!this.isSolving && (this.mouseIsDown || this.clicked) && !this.areSamePos(node, {pos: this.start}) && !this.areSamePos(node, {pos: this.end})) {
       this.clearPath();
   
-      this.selected = newArr;
+      if (this.currentTileType === "walls") {
+        if (!node.wall) {
+          this.wallsTotal += 1;
+        } else {
+          this.wallsTotal -= 1;
+        }
+        
+        node.wall = !node.wall;
+      } else {
+        node.weight = !node.weight;
+      }
     }
 
     this.clicked = false;
   }
 
-  isPositionPresent(target:number[]) {
-    return this.isPresent(this.selected, target);
-  }
-
-  isPresent(array:number[][], target:number[]) {
-    return array.find(ele => this.areSamePos(ele, target)) !== undefined;
-  }
-
   clearMaze() {
     if (!this.isSolving) {
-      this.selected = [];
+      this.squares.forEach(row => 
+        row.forEach(block => Object.assign(block, defaultNode))
+      );
+
       this.start = [];
       this.end = [];
   
@@ -152,23 +186,30 @@ export class MazeComponent implements OnInit {
 
   clearPath() {
     if (!this.isSolving) {
-      this.seen = [];
-      this.finalPath = [];
-      this.possiblePositions = [];
-      this.branchingPaths = {};
+      this.squares.forEach(row => 
+        row.forEach(block => Object.assign(block, {...defaultNode, wall: block.wall, weight: block.weight}))
+      );
+
+      this.openList = [];
+      this.currentNode = {pos: [], ...defaultNode};
+      this.seenTotal = 0;
+      this.finalTotal = 0;
+      this.totalSteps = 0;
+      this.timeTaken = 0;
+      this.seenTotalPercentage = 0;
     }
   }
 
-  placeGoal(array:number[], goal:string) {
+  placeGoal(node:NodeObj, goal:string) {
     const isStart:boolean = goal === "start";
-    const getGoal = (isStartPos:boolean) =>  isStartPos ? this.start : this.end;
-    const setGoal = (goal:number[]) => isStart ? this.start = goal : this.end = goal;
+    const getGoal = (isStartPos:boolean) =>  isStartPos ? (this.start) : (this.end);
+    const setGoal = (goal:number[]) => isStart ? (this.start = goal) : (this.end = goal);
 
-    if (!this.isSolving && !this.isPositionPresent(array) && !this.areSamePos(getGoal(!isStart), array)) {
-      if (this.areSamePos(getGoal(isStart), array)) {
+    if (!this.isSolving && !node.wall && !node.weight && !this.areSamePos({pos: getGoal(!isStart)}, node)) {
+      if (this.areSamePos({pos: getGoal(isStart)}, node)) {
         setGoal([]);
       } else {
-        setGoal(array);
+        setGoal(node.pos);
       }
 
       this.clearPath(); 
@@ -203,14 +244,23 @@ export class MazeComponent implements OnInit {
   }
 
   findNeighbors(position:number[]) {
-    const directions = [[1, 0], [0, 1], [-1, 0], [0, -1]];
-    const neighbors:number[][] = [];
+    const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+    const neighbors:NodeObj[] = [];
 
     directions.forEach(direction => {
-      const neighbor = [(direction[0] + position[0]), (direction[1] + position[1])];
-      
-      if ((neighbor[0] >= 0 && neighbor[0] <= (this.height - 1) && neighbor[1] >= 0 && neighbor[1] <= (this.width - 1)) && !this.isPositionPresent(neighbor)) {
-        neighbors.push(neighbor);
+      const neighborPos = [(direction[0] + position[0]), (direction[1] + position[1])];
+
+      if (neighborPos[0] >= 0 && neighborPos[0] <= (this.height - 1) && neighborPos[1] >= 0 && neighborPos[1] <= (this.width - 1)) {
+        const neighborNode = this.squares[neighborPos[0]][neighborPos[1]];
+
+        // if the neighborPos is within the maze boundaries and it is not a wall or closed, push it into the neighbors array
+        if (!neighborNode.wall && !neighborNode.closed) {
+          if (this.algorithm !== "A*") {
+            neighbors.push(neighborNode);
+          } else {
+            neighbors.unshift(neighborNode);
+          }
+        }
       }
     });
 
@@ -218,123 +268,155 @@ export class MazeComponent implements OnInit {
   }
 
   filterNeighbors() {
-    const adjacentSquares = this.findNeighbors(this.currentPosition);
-    
-    adjacentSquares.forEach(neighbor => {
-      if (!(this.isPresent(this.seen, neighbor) || this.isPresent(this.possiblePositions, neighbor))) {
-        this.possiblePositions.push(neighbor);
-        this.branchingPaths[neighbor.toString()] = this.currentPosition;
+    const adjacentSquares = this.findNeighbors(this.currentNode.pos);
+
+    for (let index = 0; index < adjacentSquares.length; index++) {
+      const neighborNode = adjacentSquares[index];
+
+      const isNotInQueue = !this.isPresent(this.openList, neighborNode);
+
+      if (this.algorithm === "Breadth First Search") {
+        if (isNotInQueue) {
+          neighborNode.parent = this.currentNode;
+          this.openList.push(neighborNode);
+        }
+      } else {
+        // if the neighbor is weighted, make sure to take it into account
+        const tempGScore = this.currentNode.g + this.distanceFromTo(neighborNode, this.currentNode) + (neighborNode.weight ? (neighborNode.g + 20) : (0));
+
+        if (isNotInQueue) {
+          if (this.algorithm !== "A*") {
+            this.openList.push(neighborNode);
+          } else {
+            this.openList.unshift(neighborNode);
+          }
+        // no need to update the neighborNode if the tempGScore is not lower than neighborNode.g (a better path was not found)
+        // end the element here if bottom is true
+        } else if (tempGScore >= neighborNode.g) {
+            continue;
+        }
+  
+        // gscore = distance between the current node and the neighboring node
+        neighborNode.g = tempGScore;
+  
+
+        if (this.algorithm === "A*") {
+          // hscore = estimated distance from the current node to the end node.
+          neighborNode.h = this.distanceFromTo(neighborNode, this.getEnd()); 
+        }
+  
+        // f = g + h
+        // lowest fscore is best path
+        neighborNode.f = this.algorithm === "A*" ? (neighborNode.g + neighborNode.h) : (neighborNode.g);
+  
+        // the current node will be the parent of its neighbors
+        neighborNode.parent = this.currentNode;
       }
-    });
+
+      if (isNotInQueue) {
+        neighborNode.seen = true;
+        this.seenTotal += 1;
+      }
+    }
   }
 
   async solve() {
     if (this.start.length === 2 && this.end.length === 2 && !this.isSolving) {
+      const performance = window.performance;
+
+      const startTime = performance.now()
+
       this.clearPath();
 
-      this.isSolving = true;
-      this.currentPosition = this.start;
-      this.possiblePositions = [this.start];
-      this.seen = [this.start];
-  
-      while (this.possiblePositions.length !== 0 && !this.areSamePos(this.currentPosition, this.end)) {
+      this.seenTotal += 1;
 
+      const current = this.getStart();
+
+      this.isSolving = true;
+      this.currentNode = current;
+      this.openList = [current];
+
+      // the starting position will always be the best position
+      this.currentNode.f = 0;
+      this.currentNode.g = 0;
+  
+      while (this.openList.length !== 0 && !this.areSamePos(this.currentNode, {pos: this.end})) {
         if (this.speed !== 0) {
           let delayRes = await delay(this.speed);
         }
 
+        if (!["Breadth First Search"].includes(this.algorithm)) {
+          if (this.algorithm === "A*") {
+            // a* algorithm (informed) f(n) = g(n) + h(n)
+            this.currentNode = this.bestNodeChoice(this.openList, (node:NodeObj) => node.f);
+          } else if (this.algorithm === "Dijkstra") {
+            // Dijkstra’s algorithm (uninformed) f(n) = g(n)
+            this.currentNode = this.bestNodeChoice(this.openList, (node:NodeObj) => node.g);
+          }
 
-        if (this.algorithm === "A*") {
-          // a* algorithm (informed)
-          this.currentPosition = this.bestPosFScore(this.possiblePositions);
+          const posInd = this.openList.findIndex(ele => this.areSamePos(ele, this.currentNode));
+          this.openList.splice(posInd, 1);
         } else {
-          // Dijkstra’s algorithm (uninformed)
-          this.currentPosition = this.reduceArray(this.possiblePositions, (position:number[]) => this.findPath(position).length);
+          if (this.algorithm === "Breadth First Search") {
+            this.currentNode = this.openList.shift()!;
+          }
         }
 
-        const posInd = this.possiblePositions.findIndex(ele => this.areSamePos(ele, this.currentPosition));
-        
-        this.possiblePositions.splice(posInd, 1);
-
-        this.seen.push(this.currentPosition);
-
+        this.currentNode.closed = true;
         this.filterNeighbors();
       }
-  
-      this.finalPath = this.findPath();
       
-      this.isSolving = false;
-    }
-  }
+      this.timeTaken = Math.trunc(performance.now() - startTime);
 
-  bestPosFScore(positions:number[][]) {
-    // pick the position with the best fScore
+      let currentNode = this.getEnd();
 
-    return this.reduceArray(positions, (position:number[]) => this.manhattanDistance(position));
-  }
+  
+      while (currentNode.parent !== undefined) {
+        let delayRes = await delay(10);
+        currentNode = currentNode.parent;
+        currentNode.final = true;
+        
+        this.finalTotal += 1;
 
-  reduceArray(array:number[][], reducer:Function) {
-    return array.reduce((prevPos, currentPos) => {
-      const oldFScore = reducer(prevPos);
-      const newFScore = reducer(currentPos);
-
-      if (oldFScore > newFScore) {
-        return currentPos;
-      } else {
-        return prevPos;
+        if (currentNode.weight) {
+          this.totalSteps = this.totalSteps + 21;
+        } else {
+          this.totalSteps += 1;
+        }
       }
-    });
+
+      this.isSolving = false;    
+    }
   }
 
-  manhattanDistance(pos:number[]) {
-    // f = g + h
+  bestNodeChoice(positions:NodeObj[], determineValue:Function) {
+    // pick the position with the best fScore
+    let lowInd = 0;
 
-    // lowest fscore is best path
-
-    return (
-      // gscore = distance between the current node and the start node
-      this.findPath(pos).length 
-      + 
-      // hscore = estimated distance from the current node to the end node.
-      (Math.abs(pos[0] - this.end[0]) + Math.abs(pos[1] - this.end[1]))
-    );
-  }
-
-  findPath(goal = this.end) {
-    const path:number[][] = [goal];
-    let spot:string = goal.toString();
-
-    while (this.branchingPaths[spot] !== undefined) {
-      // keep pushing the positions until we hit undefined
-
-      // this loop works by using the value of the current goal to give us the path that led us from the starting position to the current goal
-
-      // if our goal is [12, 27], we will start off of [12, 27] which the value then leads us to [12, 26]
-      // which then leads us to the key [12, 26] which then gives us the value [12, 25] and then we keep doing this 
-      // working our way backwards until we reach a key-value pair where the value is not a key in branchingPaths 
-      // which means we've reached the beginning (or starting position)
-      // {
-      //   [12,14]: [11, 14],
-      //   [12,15]: [12, 14],
-      //   [12,16]: [12, 15],
-      //   [12,17]: [12, 16],
-      //   [12,18]: [12, 17],
-      //   [12,19]: [12, 18],
-      //   [12,20]: [12, 19],
-      //   [12,21]: [12, 20],
-      //   [12,22]: [12, 21],
-      //   [12,23]: [12, 22],
-      //   [12,24]: [12, 23],
-      //   [12,25]: [12, 24],
-      //   [12,26]: [12, 25],
-      //   [12,27](goal): [12, 26]
-      // }
-
-      path.push(this.branchingPaths[spot]);
-      spot = this.branchingPaths[spot].toString();
+    for (let index = 0; index < positions.length; index++) {
+      if (determineValue(positions[index]) < determineValue(positions[lowInd])) { 
+        lowInd = index; 
+      }
     }
 
-    // the returned value will tell us what positions were taken to reach the current goal
-    return path;
+    return positions[lowInd];
+  }
+
+  getStart() {
+    if (this.start.length === 2) {
+      return this.squares[this.start[0]][this.start[1]];
+    }
+    return {...defaultNode, pos: []};
+  }
+
+  getEnd() {
+    if (this.end.length === 2) {
+      return this.squares[this.end[0]][this.end[1]];
+    }
+    return {...defaultNode, pos: []};
+  }
+
+  distanceFromTo(from:NodeObj, to:NodeObj) {
+    return Math.abs(from.pos[0] - to.pos[0]) + Math.abs(from.pos[1] - to.pos[1]);
   }
 }
